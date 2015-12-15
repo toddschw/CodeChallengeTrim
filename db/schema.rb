459956d7e730d16c7e8b0fe3cf95982cb0bc030a
Todd Schwartzfarb <todd@wirestreak.com>
2015-12-15 15:07:33 -0500

    created database because we changed database name

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151215000614) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "fullname"
     t.string   "email"
